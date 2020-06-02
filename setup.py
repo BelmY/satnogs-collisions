052f61ad12210898c5b5205dfab1f724cd8c247d
RavicharanN <ravicharan.vsp@gmail.com>
2020-06-02 17:39:49 +0530

    Initialize project
    Signed-off-by: Ravi Charan <ravicharan.vsp@gmail.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='satnogs-collisions',
+	version='0.1.0',
+	url='https://gitlab.com/librespacefoundation/satnogs/satnogs-collisions',
+	license='AGPLv3',
+	author = 'SatNOGS project'
+	author_email = 'dev@satnogs.org'
+	packages=['satnogs-collisions'],
+	classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU Affero General Public License v3",
+      ],
+	)
